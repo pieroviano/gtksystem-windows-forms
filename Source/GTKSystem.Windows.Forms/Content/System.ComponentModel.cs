c6d137e550d93695923a2afa9dc02e460b4e6a8d
pieroviano <piero.viano@gmail.com>
2025-05-29 04:29:15 +0300

    ''

--- Source/GTKSystem.Windows.Forms/Content/System.ComponentModel.cs
+++ Source/GTKSystem.Windows.Forms/Content/System.ComponentModel.cs
@@ -1,4 +1,5 @@
-﻿// This file is mainly to overwrite the native System.ComponentModel.ComponentResourceManager class. The native class does not support reading project resource image files.
+﻿#if !ExcludeResources
+// This file is mainly to overwrite the native System.ComponentModel.ComponentResourceManager class. The native class does not support reading project resource image files.
 // GTKSystem.ComponentModel.ComponentResourceManager implements reading of project resource files and image files.
 // If the resource image file is not used in the project, there is no need to create this file.
 
@@ -37,3 +38,4 @@ internal class ComponentResourceManager : GtkComponentResourceManager
         return (T?)base.GetObject(name);
     }
 }
+#endif
--- Source/GTKSystem.Windows.Forms/Content/System.Resources.ResourceManager.cs
+++ Source/GTKSystem.Windows.Forms/Content/System.Resources.ResourceManager.cs
@@ -1,4 +1,5 @@
-﻿// This file is mainly to overwrite the native System.Resources.ResourceManager class.
+﻿#if !ExcludeResources
+// This file is mainly to overwrite the native System.Resources.ResourceManager class.
 // The native class does not support reading project resource image files.
 // GTKSystem.Resources.ResourceManager implements reading of project resource files and image files.
 // If the resource image file is not used in the project, there is no need to create this file.
@@ -30,3 +31,4 @@ public class ResourceManager : System.Resources.GtkResourceManager
 
     }
 }
+#endif
